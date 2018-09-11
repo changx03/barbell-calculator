ccd0191d518f2fbd444d5fc56765138e6d0b25d5
Luke Chang <luke@csel.co.nz>
2018-09-12 09:09:39 +1200

    added githubicon; updated footer

--- src/views/components/AppFooter.tsx
+++ src/views/components/AppFooter.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Typography } from '@material-ui/core';
+import { GitHubIcon } from './GitHubIcon';
 
 export const AppFooter = () => (
   <div id="footer" role="contentinfo" style={{ textAlign: 'center' }}>
@@ -7,5 +8,6 @@ export const AppFooter = () => (
       Released under the <a href="https://github.com/mui-org/material-ui/blob/master/LICENSE/">MIT License</a>.
     </Typography>
     <Typography id="footer-author" variant="body1">Developed by Luke Chang, 2018</Typography>
+    <a href="https://github.com/changx03/barbell-calculator" style={{ color: 'inherit' }}><GitHubIcon/></a>
   </div>
 );
